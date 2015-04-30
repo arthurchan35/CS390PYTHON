e1f11aaa3aab3664223b207500f31caf7c70d9ba
Joseph Emmons <joeyemmons@yahoo.com>
2015-04-30 02:14:48 -0400

    Def Patches

--- apache/htdocs/htmlPatcher.py
+++ apache/htdocs/htmlPatcher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import datetime
+
 
 def patch_html(file, patches):
     s = open(file).read()
@@ -7,3 +9,13 @@ def patch_html(file, patches):
     for x in patches:
         s = s.replace(x[0], str(x[1]))
     return s
+
+
+def default_patches():
+    p = [
+        ["%date", datetime.datetime.now().strftime("%x")],
+        ["%time", datetime.datetime.now().strftime("%X")],
+
+
+        ]
+    return p
