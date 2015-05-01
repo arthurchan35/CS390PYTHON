636d6a3edd5498449c2422306973c7a8ae269d3f
Joseph Emmons <joeyemmons@yahoo.com>
2015-05-01 13:25:08 -0400

    fggv
    asdasdasd

--- apache/htdocs/postGenerator.py
+++ apache/htdocs/postGenerator.py
@@ -14,24 +14,24 @@ def genTextPost(x, y, text):
 	return htmlPatcher.patchPage("textPost.t", p)
 
 def genMain(circleDir):
-	m = "test  " + circleDir
+	m = "<p>test  " + circleDir
 
 	import glob
 	l = glob.glob(circleDir + "*")
 	file_date_tuple_list = [(x,os.path.getmtime(x)) for x in l]
 	file_date_tuple_list.sort(key=lambda x: x[1])
 	l = [x[0] for x in file_date_tuple_list]
-	test += len(l)
+	m += len(l)
 	i = 0
 	for y in range(0,6):
 		for x in range(0,4):
 			id = y*4 + x
-			if id >= len(l): return m
+			if id >= len(l): return m + "</p>"
 
 			t = l[id]
 			if t.endswith(".txt"):
 				m += genTextPost(x, y, open(t).read())
 			elif t.endswith(".jpg") or t.endswith(".png") or t.endswith(".gif"):
 				m += genPicPost(x, y, t)
-	return m
+	return m + "</p>"
 
