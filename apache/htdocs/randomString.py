b56d7ccc524adb3fe177204bf3cc72dbbbec8d50
arthurchan35 <arthurchan35@gmail.com>
2015-04-30 20:51:57 -0400

    asdb

--- apache/htdocs/randomString.py
+++ apache/htdocs/randomString.py
@@ -3,6 +3,3 @@ import string
 import random
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
-
-randstr = id_generator()
-print randstr
