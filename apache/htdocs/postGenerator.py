45226f17a07d55861cced69cd3f666a605504ff5
Joseph Emmons <joeyemmons@yahoo.com>
2015-05-01 13:15:12 -0400

    sdfsdfs

--- apache/htdocs/postGenerator.py
+++ apache/htdocs/postGenerator.py
@@ -14,14 +14,14 @@ def genTextPost(x, y, text):
 	return htmlPatcher.patchPage("textPost.t", p)
 
 def genMain(circleDir):
-	m = ""
+	m = "test  " + circleDir
 
 	import glob
 	l = glob.glob(circleDir + "*")
 	file_date_tuple_list = [(x,os.path.getmtime(x)) for x in l]
 	file_date_tuple_list.sort(key=lambda x: x[1])
 	l = [x[0] for x in file_date_tuple_list]
-
+	test += len(l)
 	i = 0
 	for y in range(0,6):
 		for x in range(0,4):
